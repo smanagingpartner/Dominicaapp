aff02b3eda3df6678c84b7645a748002229d6f7f
Smanagingpartner <74501932+smanagingpartner@users.noreply.github.com>
2022-04-29 10:21:14 -0400

    Update Demo111.py

--- Demo111.py
+++ Demo111.py
@@ -2,6 +2,7 @@ import json
 
 import requests
 import streamlit as st
+from streamlit_lottie import st_lottie
 from PIL import Image
 
 st.set_page_config(page_title = 'My Webpage', page_icon = 'tada', layout = 'wide')
